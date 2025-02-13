8fdf07e52afcd9f2edcf18f118c0d11c366a3d4c
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-02-13 12:57:11 +0300

    task#15

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,11 @@
-<script>
-export default {
-  data() {
-    return {
-      name: 'John',
-      surn: 'Smit',
-      text: 'page',
-      href: 'page.html',
-    };
-  },
-};
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
     <TheWelcome />
-    <div>
-      <h2>Привет!</h2>
-      <p>Я - <strong>{{ name }}</strong>, студент!</p>
-      <div>{{ name }}</div>
-      <div>{{ surn }}</div>
-      <a :href="href">{{ text }}</a>
-    </div>
   </main>
 </template>
 
