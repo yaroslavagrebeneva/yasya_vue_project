b6c75e0d3caf48021f67d3afe6b70615b9660991
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-02-17 16:50:57 +0300

    task#33

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { ref } from 'vue';
+
+// Task#33 - Filtering Positive Numbers
+const items = ref([1, -2, 3, -4, 5]);
 </script>
 
 <template>
   <div>
-    <h1>Task#32 - Numbered Paragraphs</h1>
-    <p v-for="num in 30" :key="num">
-      {{ num }}
-    </p>
+    <h1>Task#33 - Positive Numbers</h1>
+    <ul>
+      <template v-for="item in items">
+        <li v-if="item > 0" :key="item">
+          {{ item }}
+        </li>
+      </template>
+    </ul>
   </div>
 </template>
