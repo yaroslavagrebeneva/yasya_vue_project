4c883ea4abbcdb7583ef96ea52505424d935b873
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-02-17 13:25:58 +0300

    task#28

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,17 +1,21 @@
 <script setup>
 import { ref } from 'vue';
 
-// Task#27 - тоггл абзаца с v-show
-const isParagraphVisible = ref(true);
-const toggleParagraph = () => {
-  isParagraphVisible.value = !isParagraphVisible.value;
-};
+// Task#27 - Перебор массива и вывод элементов в div
+const items = ref([1, 2, 3, 4, 5]);
+
+// Task#28 - Вывод ключей массива в списке ul
+const arr = ref(['x', 'y', 'z']);
 </script>
 
 <template>
   <div>
     <h1>Task#27</h1>
-    <button @click="toggleParagraph">{{ isParagraphVisible ? 'Скрыть' : 'Показать' }} абзац</button>
-    <p v-show="isParagraphVisible">Этот абзац можно скрыть или показать</p>
+    <div v-for="item in items" :key="item">{{ item }}</div>
+
+    <h1>Task#28</h1>
+    <ul>
+      <li v-for="(elem, key) in arr" :key="key">{{ key }}</li>
+    </ul>
   </div>
 </template>
