a1caab9e9fca6a7e67921992ad831d97d1beeadf
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-03 12:30:09 +0300

    task#48

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,25 +1,29 @@
 <script setup>
 import { ref } from 'vue';
 
-// Task#47_1 & Task#47_2 - Checkbox group handling
-const selectedLanguages = ref([]);
-const languages = ['JavaScript', 'Python', 'Java', 'C++', 'Ruby'];
+// Task#48_1 & Task#48_2 - Radio button handling
+const selectedLanguage = ref('');
+const languages = [
+  { name: 'English', text: 'Hello, how are you?' },
+  { name: 'Spanish', text: 'Hola, ¿cómo estás?' },
+  { name: 'French', text: 'Bonjour, comment ça va?' }
+];
 </script>
 
 <template>
   <div class="styled-container">
-    <h1>Task#47 - Checkbox Group</h1>
+    <h1>Task#48 - Radio Button Selection</h1>
     
-    <!-- Task#47_1: Checkbox group for selecting languages -->
-    <p>Select known languages:</p>
-    <div v-for="lang in languages" :key="lang">
-      <input type="checkbox" v-model="selectedLanguages" :value="lang"> {{ lang }}
+    <!-- Task#48_1: Radio button group for selecting a native language -->
+    <p>Select your native language:</p>
+    <div v-for="lang in languages" :key="lang.name">
+      <input type="radio" v-model="selectedLanguage" :value="lang.name"> {{ lang.name }}
     </div>
     
-    <!-- Task#47_2: Display selected languages in a list -->
-    <ul>
-      <li v-for="lang in selectedLanguages" :key="lang">{{ lang }}</li>
-    </ul>
+    <!-- Task#48_2: Display corresponding phrase based on selection -->
+    <p v-if="selectedLanguage">
+      {{ languages.find(lang => lang.name === selectedLanguage)?.text }}
+    </p>
   </div>
 </template>
 
