ed49cd64f5b5063d2427d8fb873d6e9a887a16fb
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-03 12:22:10 +0300

    task#42

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,16 +1,26 @@
 <script setup>
 import { ref } from 'vue';
 
-// Task#41 - Dynamic Class Binding
+// Task#41 - Dynamic Style Binding
 const isValid = ref(true);
 const isDisabled = ref(true);
+
+// Task#42_1 - Green text, yellow background
+const textStyle1 = ref({ color: 'green', backgroundColor: 'yellow' });
+
+// Task#42_2 - Bold and italic text
+const textStyle2 = ref({ fontWeight: 'bold', fontStyle: 'italic' });
 </script>
 
 <template>
   <div class="styled-container">
-    <h1>Task#41 - Dynamic Class Binding</h1>
-    <p :class="{ valid: isValid, disabled: isDisabled }">
-      This paragraph has dynamic class binding.
+    <h1>Task#41 - Dynamic Style Binding</h1>
+    <p :class="{ valid: isValid, disabled: isDisabled }" 
+       :style="textStyle1">
+      This paragraph has green text and yellow background.
+    </p>
+    <p :style="textStyle2">
+      This paragraph has bold and italic text.
     </p>
   </div>
 </template>
