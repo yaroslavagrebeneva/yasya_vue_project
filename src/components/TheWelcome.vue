6bb19aa9fe7d92735ff45045293acba1756adeb7
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-07 16:34:03 +0300

    task#56

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -7,6 +7,7 @@
           <th>Salary</th>
           <th>Age</th>
           <th>Action</th>
+          <th>Edit</th>
         </tr>
       </thead>
       <tbody>
@@ -17,9 +18,32 @@
           <td>
             <a href="#" @click="removeUser(index)">Delete</a>
           </td>
+          <td>
+            <a href="#" @click="editUser(index)">Edit</a>
+          </td>
         </tr>
       </tbody>
     </table>
+
+    <!-- Modal for Editing -->
+    <div v-if="isEditing" class="modal">
+      <div class="modal-content">
+        <h3>Edit User</h3>
+        <form @submit.prevent="saveChanges">
+          <label for="name">Name:</label>
+          <input type="text" v-model="editedUser.name" />
+
+          <label for="salary">Salary:</label>
+          <input type="number" v-model="editedUser.salary" />
+
+          <label for="age">Age:</label>
+          <input type="number" v-model="editedUser.age" />
+
+          <button type="submit">Save</button>
+          <button @click="cancelEdit">Cancel</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,12 +56,56 @@ export default {
         { id: 2, name: 'name2', salary: 200, age: 40 },
         { id: 3, name: 'name3', salary: 300, age: 50 },
       ],
+      isEditing: false,
+      editedUser: null,
+      editedUserIndex: null,
     };
   },
   methods: {
     removeUser(index) {
       this.users.splice(index, 1);
     },
+    editUser(index) {
+      this.isEditing = true;
+      this.editedUserIndex = index;
+      this.editedUser = { ...this.users[index] };
+    },
+    saveChanges() {
+      if (this.editedUser) {
+        this.users[this.editedUserIndex] = { ...this.editedUser };
+        this.cancelEdit();
+      }
+    },
+    cancelEdit() {
+      this.isEditing = false;
+      this.editedUser = null;
+      this.editedUserIndex = null;
+    },
   },
 };
 </script>
+
+<style>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  width: 300px;
+}
+
+button {
+  margin-top: 10px;
+}
+</style>
