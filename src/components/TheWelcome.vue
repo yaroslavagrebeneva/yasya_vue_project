156b187da3d61e0f5ab0f553e53921b264700b26
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-07 16:33:14 +0300

    task#55

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(item, index) in items" :key="index" @click="removeItem(index)">
-        {{ item }}
-      </li>
-    </ul>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Salary</th>
+          <th>Age</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in users" :key="user.id">
+          <td>{{ user.name }}</td>
+          <td>{{ user.salary }}</td>
+          <td>{{ user.age }}</td>
+          <td>
+            <a href="#" @click="removeUser(index)">Delete</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -12,12 +27,16 @@
 export default {
   data() {
     return {
-      items: ['item 1', 'item 2', 'item 3', 'item 4'],
+      users: [
+        { id: 1, name: 'name1', salary: 100, age: 30 },
+        { id: 2, name: 'name2', salary: 200, age: 40 },
+        { id: 3, name: 'name3', salary: 300, age: 50 },
+      ],
     };
   },
   methods: {
-    removeItem(index) {
-      this.items.splice(index, 1);
+    removeUser(index) {
+      this.users.splice(index, 1);
     },
   },
 };
